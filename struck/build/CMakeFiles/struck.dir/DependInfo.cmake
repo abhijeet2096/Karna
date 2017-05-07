c70dfd87a02a5282912cf05cacfd820be69c19ef
indresh07 <indresh768@gmail.com>
2017-05-07 17:14:46 +0530

    project set up

---
+++ struck/build/CMakeFiles/struck.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/indresh/struck/struck/src/Config.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/Config.cpp.o"
+  "/home/indresh/struck/struck/src/Features.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/Features.cpp.o"
+  "/home/indresh/struck/struck/src/GraphUtils/GraphUtils.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/GraphUtils/GraphUtils.cpp.o"
+  "/home/indresh/struck/struck/src/HaarFeature.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/HaarFeature.cpp.o"
+  "/home/indresh/struck/struck/src/HaarFeatures.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/HaarFeatures.cpp.o"
+  "/home/indresh/struck/struck/src/HistogramFeatures.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/HistogramFeatures.cpp.o"
+  "/home/indresh/struck/struck/src/ImageRep.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/ImageRep.cpp.o"
+  "/home/indresh/struck/struck/src/LaRank.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/LaRank.cpp.o"
+  "/home/indresh/struck/struck/src/MultiFeatures.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/MultiFeatures.cpp.o"
+  "/home/indresh/struck/struck/src/RawFeatures.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/RawFeatures.cpp.o"
+  "/home/indresh/struck/struck/src/Sampler.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/Sampler.cpp.o"
+  "/home/indresh/struck/struck/src/Tracker.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/Tracker.cpp.o"
+  "/home/indresh/struck/struck/src/main.cpp" "/home/indresh/struck/struck/build/CMakeFiles/struck.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "../src"
+  "/usr/local/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
